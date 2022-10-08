c05eeda5c86c05606bce8d09feea514c9564126b
Sumit <sumit.123sahni@gmail.com>
2022-10-08 23:56:55 +0530

    Updated

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-// import { useEffect, useState } from "react";
-// import { useDispatch, useSelector} from "react-redux";
-// import { getCloth} from "../Redux/API/clothSlice"
 import FetchClothApi from './FetchClothApi';
 
 
